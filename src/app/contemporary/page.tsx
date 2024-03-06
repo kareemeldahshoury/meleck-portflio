df89587e9f77640415d996413629fd8af110886f
kareemeldahshoury <eldah001@umn.edu>
2024-03-06 15:40:59 -0600

    contemp order

--- src/app/contemporary/page.tsx
+++ src/app/contemporary/page.tsx
@@ -5,9 +5,9 @@ import Gallery from './gallery';
 
 export default function Home() {
 const images = [
+    { src: '/mel.jpg', alt: 'Description of image 2' },
     { src: '/44.5.jpg', alt: 'Description of image 1' },
     { src: '/lucia.jpg', alt: 'Description of image 2' },
-    { src: '/mel.jpg', alt: 'Description of image 2' },
     { src: '/menay6.jpg', alt: 'Description of image 2' },
     { src: '/DSC00889.JPG', alt: 'Description of image 2' },
     { src: '/DSC05114.JPG', alt: 'Description of image 2' },
