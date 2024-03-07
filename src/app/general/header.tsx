f4a57e878d2a08e25382d18fbfe8b121ae0f218b
kareemeldahshoury <eldah001@umn.edu>
2024-03-06 19:50:17 -0600

    header flex

--- src/app/general/header.tsx
+++ src/app/general/header.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import Link from 'next/link'; 
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className="bg-custom-blue text-white font-average body-font shadow w-full">
-      <div className="flex w-full p-5 flex-col md:flex-row items-center justify-between">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
         <Link href="/" className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-            <span className="text-3xl text-white  font-average">Meleck Eldahshoury</span>
+            {/* Adjust the text size for smaller screens using responsive prefixes */}
+            <span className="text-xl md:text-2xl text-white font-average">Meleck Eldahshoury</span>
         </Link>
-        <nav className="text-white ">
-          <Link href="/"className="mr-5 hover:text-gray-900 text-xl">Home</Link>
-          <Link href="/contemporary" className="mr-5 hover:text-gray-900 text-xl">Contemporary</Link>
-          <Link href="/portraits" className="mr-5 hover:text-gray-900 text-xl">Portrait</Link>
-          <Link href="/sports" className="mr-5 hover:text-gray-900 text-xl">Sports</Link>
-          <Link href="/about" className="mr-5 hover:text-gray-900 text-xl">About</Link>
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          {/* Consider hiding or changing the layout of these links on smaller screens */}
+          <Link href="/" className="mr-5 hover:text-gray-900 text-lg md:text-xl">Home</Link>
+          <Link href="/contemporary" className="mr-5 hover:text-gray-900 text-lg md:text-xl">Contemporary</Link>
+          <Link href="/portraits" className="mr-5 hover:text-gray-900 text-lg md:text-xl">Portrait</Link>
+          <Link href="/sports" className="mr-5 hover:text-gray-900 text-lg md:text-xl">Sports</Link>
+          <Link href="/about" className="mr-5 hover:text-gray-900 text-lg md:text-xl">About</Link>
         </nav>
       </div>
     </header>
